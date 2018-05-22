78d9a8d53d2f25ede11771921a9e64f6cb3b3084
yaoshifeng <1320701765@qq.com>
2018-05-22 10:41:34 +0800

    Update settings.py

--- xiaoshuo/xiaoshuo/settings.py
+++ xiaoshuo/xiaoshuo/settings.py
@@ -64,9 +64,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'xiaoshuo.pipelines.XiaoshuoPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'xiaoshuo.pipelines.JsonInfoPipeline': 300,
+    'xiaoshuo.pipelines.JsonContentPipeline': 400,
+    # 'xiaoshuo.pipelines.DgPipeline': 500,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
